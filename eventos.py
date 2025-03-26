f20cbcd8effc41bb87c08e58006e4d7e6cbbf297
Irving2355 <it235597@gmail.com>
2025-03-26 16:26:52 -0600

    Taller 26/03/2025

--- eventos.py
+++ eventos.py
@@ -1,11 +1,18 @@
 import pygame
+import random 
 
 #inicilizar pygame
 pygame.init()
 
 WIDTH, HEIGHT = 800,600 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Mi primer juego")
+pygame.display.set_caption("Cambia de color")
+
+def random_color():
+    return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+
+#color inicial
+color = (255,255,255)
 
 #bucle principal
 
@@ -14,10 +21,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running=False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            color=random_color()
         
         #Rellenar la pantalla con color
-        screen.fill((0,0,0))
-        
+        screen.fill(color)
         #actualizar la pantalla
         pygame.display.flip()
 #salir del juego
